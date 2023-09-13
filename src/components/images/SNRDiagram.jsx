711bea73ab0387859c0ab2bf90095d40df476a77
Andy Lower <andy.lower@applow.com>
2023-09-13 18:34:15 +1000

    Update SNRDiagram.jsx

--- src/components/images/SNRDiagram.jsx
+++ src/components/images/SNRDiagram.jsx
@@ -3,7 +3,7 @@ import img from '../../images/snr_diagram.png'
 import ImageLightbox from './Lightbox';
 
 const WorkflowDiagram = () => {
-    return <ImageLightbox src={img.src} alt="Scenario Diagram" width={1200} height={800} />
+    return <ImageLightbox src={img.src} alt="Scenario Diagram" className="w-full" width={1500} height={1500} />
 };
 
 export default WorkflowDiagram;
